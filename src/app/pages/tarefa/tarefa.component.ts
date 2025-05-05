e606e0deb2aa192acb2acf3cf0c6f6ece7a25ec0
JoaoVBelladona <joao.2023004434@aluno.iffar.edu.br>
2025-05-05 19:58:34 -0300

    fodase

--- src/app/pages/tarefa/tarefa.component.ts
+++ src/app/pages/tarefa/tarefa.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import { TarefaService } from '../../services/tarefa.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-tarefa',
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule, 
+  MatRadioModule, FormsModule,],
   templateUrl: './tarefa.component.html',
   styleUrl: './tarefa.component.scss'
 })
 export class TarefaComponent {
+
+  titulo:string = '';
+  descricao:string = '';
+  concluido:boolean = false;
+
+  constructor(private tarefaService:TarefaService){
+  }
+
+  gravar(){
+    this.tarefaService.addTarefa({
+      titulo:this.titulo, 
+      descricao:this.descricao, 
+      concluida:this.concluido,
+    });
+    this.titulo = '';
+    Swal.fire({
+      icon: 'success',
+      title: 'sucesso',
+      text: 'Tarefa salva com sucesso!',
+    })
+  }
 
 }
